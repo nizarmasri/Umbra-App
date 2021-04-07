6d6630e281154692925e076828e900f420145b29
charbelbou <49194691+charbelbou@users.noreply.github.com>
2021-04-07 12:00:53 +0300

    Update bookmarks.dart

--- lib/bookmarks.dart
+++ lib/bookmarks.dart
@@ -14,8 +14,51 @@ class Bookmarks extends StatefulWidget {
 }
 
 class _BookmarksState extends State<Bookmarks> {
+  Widget item(DocumentSnapshot event) {
+    return Container(
+      child: Text(
+        event.data()["title"],
+        style: TextStyle(color: Colors.red),
+      ),
+    );
+  }
+
+  List<Widget> addItems(List<QueryDocumentSnapshot> events) {
+    List<Widget> newEvents = [];
+    events.forEach((element) {
+      newEvents.add(item(element));
+    });
+    return newEvents;
+  }
+
+  String uid = FirebaseAuth.instance.currentUser.uid;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Bookmarks",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: FutureBuilder(
+          future: FirebaseFirestore.instance
+              .collection("users")
+              .doc(uid)
+              .get()
+              .then((value) {
+            return FirebaseFirestore.instance
+                .collection("events")
+                .where("__name__", whereIn: value.data()["booked"])
+                .get();
+          }),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            return Column(
+              children: addItems(snapshot.data.docs),
+            );
+          }),
+    );
   }
 }
