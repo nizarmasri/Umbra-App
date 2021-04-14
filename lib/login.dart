3044e3eb09ac42637daf52a71c1f75bea93763af
charbelbou <49194691+charbelbou@users.noreply.github.com>
2021-04-14 18:12:17 +0300

    .

--- lib/login.dart
+++ lib/login.dart
@@ -49,7 +49,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.height;
-    double btnWidth = width * 0.415;
+    double btnWidth = width * 0.5;
 
     return loader
         ? GestureDetector(
